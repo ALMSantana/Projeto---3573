e97616ce54ed2b6c3f4f7dc5054d3d0746466c64
ALMSantana <andrelms4@insper.edu.br>
2023-12-05 16:00:58 -0300

    Aula 1.4

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from time import sleep
+from helpers import *
 
 load_dotenv()
 
@@ -11,6 +12,8 @@ modelo = "gpt-4"
 
 app = Flask(__name__)
 app.secret_key = 'alura'
+
+contexto = carrega("dados/ecomart.txt")
 
 def bot(prompt):
     maximo_tentativas = 1
@@ -21,6 +24,10 @@ def bot(prompt):
             prompt_do_sistema = f"""
             Você é um chatbot de atendimento a clientes de um e-commerce. 
             Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Você deve gerar respostas utilizando o contexto abaixo.
+            
+            # Contexto
+            {contexto}
             """
             response = cliente.chat.completions.create(
                 messages=[
@@ -34,7 +41,7 @@ def bot(prompt):
                         }
                 ],
                 temperature=1,
-                max_tokens=256,
+                max_tokens=300,
                 top_p=1,
                 frequency_penalty=0,
                 presence_penalty=0,
