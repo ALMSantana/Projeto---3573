6c4a97f3d177994b125fb6b534b3b196c65129a4
ALMSantana <andrelms4@insper.edu.br>
2023-12-05 16:47:04 -0300

    Aula 3.1

---
+++ assistente_ecomart.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from time import sleep
+from helpers import *
+from selecionar_persona import *
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+contexto = carrega("dados/ecomart.txt")
+
+assistente = cliente.beta.assistants.create(
+    name="Atendente EcoMart",
+    instructions = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Além disso, adote a persona abaixo para respondero ao cliente.
+            
+            ## Contexto
+            {contexto}
+
+            ## Persona
+            {personas["neutro"]}
+            """,
+    model = modelo
+)
+
+print(assistente.id)
+
