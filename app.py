7135d1cbbc7299b307b1d8d656d7cee817abf3d9
ALMSantana <andrelms4@insper.edu.br>
2023-12-05 15:50:45 -0300

    Aula 1.3

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template, request, Response
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from time import sleep
 
 load_dotenv()
 
@@ -11,10 +12,49 @@ modelo = "gpt-4"
 app = Flask(__name__)
 app.secret_key = 'alura'
 
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+            prompt_do_sistema = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            """
+            response = cliente.chat.completions.create(
+                messages=[
+                        {
+                                "role": "system",
+                                "content": prompt_do_sistema
+                        },
+                        {
+                                "role": "user",
+                                "content": prompt
+                        }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                model = modelo)
+            return response
+        except Exception as erro:
+                repeticao += 1
+                if repeticao >= maximo_tentativas:
+                        return "Erro no GPT: %s" % erro
+                print('Erro de comunicação com OpenAI:', erro)
+                sleep(1)
+            
+
+
 @app.route("/chat", methods=["POST"])
 def chat():
     prompt = request.json["msg"]
     resposta = bot(prompt)
+    texto_resposta = resposta.choices[0].message.content
+    return texto_resposta
 
 @app.route("/")
 def home():
